173356b323333f283ba055404418966a94c19b5d
Charles Marshall <charles@oneblackbear.com>
2013-02-05 19:02:11 +0000

    register paths

---
+++ setup.php
@@ -0,0 +1,9 @@
+<?
+CMSApplication::register_module("redirect", array("display_name"=>"Redirects", "link"=>"/admin/redirect/"));
+
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+AutoLoader::$plugin_array[] = array("name"=>"wildfire.redirects","dir"=>__DIR__);
+
+?>
