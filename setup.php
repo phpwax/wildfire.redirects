309c358ff820d9648f68007d2a5be032257b168e
Charles Marshall <charles@oneblackbear.com>
2013-02-05 19:06:30 +0000

    whitespace

--- setup.php
+++ setup.php
@@ -1,6 +1,7 @@
 <?
 CMSApplication::register_module("redirect", array("display_name"=>"Redirects", "link"=>"/admin/redirect/"));
 
+
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
 AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
